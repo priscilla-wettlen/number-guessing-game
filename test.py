f16529d9c9346dbe912e4bd82b87e0207a84752a
Priscilla Silva <priscilla.sg91@gmail.com>
2022-02-17 13:18:15 +0100

    simplified test with a for loop

--- test.py
+++ test.py
@@ -1,10 +1,43 @@
 from random_game import checkGuess
-numbers_to_test = [[2, 78, "More!"], [1, 0, "Less!"], [1, 1, "Correct!"]]
+numbers_to_test = [[2, 78, "More!"], [1, 0, "Less!"], [1, 1, "Correct!"],[9, 9, "Correct!"]]
 
-listA = numbers_to_test[0][2]
-listB = numbers_to_test[1][2]
-listC = numbers_to_test[2][2]
-print(listA, listB, listC)
+for i in numbers_to_test:
+    listC = checkGuess(i[0], i[1])
+    print(listC)
+    if listC == i[2]:
+        print('Yes')
+    else:
+        print('Haha no')
+
+
+# listA = numbers_to_test[0][2]
+# listB = numbers_to_test[1][2]
+# listC = numbers_to_test[2][2]
+# print(listA, listB, listC)
+
+# listB = checkGuess(numbers_to_test[0][0], numbers_to_test[0][1])
+# if listB == numbers_to_test[0][2]:
+#     print('Yes')
+# else :
+#     print('An error has occured')
+
+
+# listB = checkGuess(numbers_to_test[1][0], numbers_to_test[1][1])
+# if listB == numbers_to_test[1][2]:
+#     print('Whee')
+# else:
+#     print('U dumb')
+
+# listB = checkGuess(numbers_to_test[2][0], numbers_to_test[2][1])
+# if listB == numbers_to_test[2][2]:
+#     print('Whee')
+# else:
+#     print('U dumb')
+
+
+
+
+
 
 
 
